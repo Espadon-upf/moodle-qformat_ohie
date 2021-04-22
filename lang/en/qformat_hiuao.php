ec52523526e391b7c75dcce9cf01240008551530
Moniteur_Tice <espadon@upf.pf>
2021-04-21 14:30:10 -1000

    Change the name of the format to 'Ohie'.

--- lang/en/qformat_hiuao.php
+++ lang/en/qformat_hiuao.php
@@ -15,18 +15,18 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'qformat_hiuao', language 'en'
+ * Strings for component 'qformat_ohie', language 'en'
  *
- * @package    qformat_hiuao
+ * @package    qformat_ohie
  * @copyright  2021 Giard Kenan <giard.kenan@hotmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Hiuao format';
+$string['pluginname'] = 'Ohie format';
 $string['pluginname_help'] = 'This is a CSV format based on XLSX file for importing questions. Please find the Sample xlsx(sample.xlsx) file sent along with this plugin for your reference.';
-$string['pluginname_link'] = 'qformat/hiuao';
+$string['pluginname_link'] = 'qformat/ohie';
 $string['commma_error'] = '<span style="color: #990000; "> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field or Put the entire text between the double quotes(" "), So the coomma(,) between them can be ignored. <br /></span>';
 $string['newline_error'] = '<span style="color: #990000; ">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been imported.</span>';
 $string['csv_file_error'] = '<span style="color: #990000; ">Upload failed. Something went wrong at line number <b/> {$a} . </b> Make sure you are uploading a valid CSV file.<br /> Please check that, <b> Simple CSV file must have 8 headers/fields and Extended CSV file must have 13 headers/fields in order work.</b><br /> Please correct the error and try importing again. <br /> No Question has been imported.</span>';
 $string['samplefile'] = 'SampleFile';
-$string['privacy:metadata'] = 'The Hiuao format plugin does not store any personal data.';
+$string['privacy:metadata'] = 'The Ohie format plugin does not store any personal data.';
--- lang/fr/qformat_hiuao.php
+++ lang/fr/qformat_hiuao.php
@@ -15,20 +15,20 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Strings for component 'qformat_csv', language 'en'
+ * Strings for component 'qformat_ohie', language 'en'
  *
- * @package    qformat_hiuao
+ * @package    qformat_ohie
  * @copyright  2021 Giard Kenan <giard.kenan@hotmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'hiuao format';
+$string['pluginname'] = 'Ohie format';
 $string['pluginname_help'] = 'Ce plugin permet d\'importer un questionnaire a partir d\'un fichier au format CSV basé 
     sur un second fichier au format XLSX. Veuillez trouver le fichier xlsx (sample.xlsx) envoyé avec ce plugin pour 
     votre référence.';
-$string['pluginname_link'] = 'qformat/hiuao';
+$string['pluginname_link'] = 'qformat/ohie';
 $string['commma_error'] = '<span style="color: #990000; "> Upload failed. Unnecessary Comma(,) found in <b> Question {$a} </b><br /> Please remove the comma(,) from the field or Put the entire text between the double quotes(" "), So the coomma(,) between them can be ignored. <br /></span>';
 $string['newline_error'] = '<span style="color: #990000; ">Upload failed. New Line found in <b> Question {$a} . </b> Make sure that entire question with choices and answers are in one line itself.<br /> Please correct this question and try importing again. <br /> No Question has been imported.</span>';
 $string['csv_file_error'] = '<span style="color: #990000; ">Upload failed. Something went wrong at line number <b/> {$a} . </b> Make sure you are uploading a valid CSV file.<br /> Please check that, <b> Simple CSV file must have 8 headers/fields and Extended CSV file must have 13 headers/fields in order work.</b><br /> Please correct the error and try importing again. <br /> No Question has been imported.</span>';
 $string['samplefile'] = 'SampleFile';
-$string['privacy:metadata'] = 'Le plugin Hiuao ne stocke pas de données personnelles.';
+$string['privacy:metadata'] = 'Le plugin Ohie ne stocke pas de données personnelles.';
